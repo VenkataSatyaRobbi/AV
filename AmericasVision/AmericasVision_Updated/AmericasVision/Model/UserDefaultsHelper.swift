8b8a8a7388efd97848b2ba474d537c61e9737686
mohan <dolamohan@gmail.com>
2018-05-26 00:16:38 +0530

    profile ui changes 2nd commit

---
+++ AmericasVision/AmericasVision_Updated/AmericasVision/Model/UserDefaultsHelper.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  AmericasVision
+//
+//  Created by Mohan Dola on 22/05/18.
+//  Copyright © 2018 zeroGravity. All rights reserved.
+//
+
+import Foundation
---
+++ AmericasVision/AmericasVision_Updated/AmericasVision/Model/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  AmericasVision
+//
+//  Created by Mohan Dola on 23/05/18.
+//  Copyright © 2018 zeroGravity. All rights reserved.
+//
+
+import Foundation
